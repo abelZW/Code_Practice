cd9266ab8ce0d7890466178b39ebf649148613fc
abelZW <15929967966@163.com>
2019-05-27 19:49:25 +0800

    接雨水问题

--- Leet_Code/Abel_test.cpp
+++ Leet_Code/Abel_test.cpp
@@ -1,11 +1,39 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <map>
+#include<iostream>
+#include<string>
 using namespace std;
-
 int main()
 {
-
+	string s;
+	int pos_p,pos_t,count_p,count_a,count_t,n;
+	scanf("%d\n",&n);
+    int flag[n];
+	for(int num = 0; num < n; num++)
+    {
+		pos_p=pos_t=count_p=count_a=count_t=0;
+		cin >> s;
+		for(int i=0;i<s.length();i++){
+			if(s[i] == 'P'){
+				count_p++;
+				pos_p = i;
+			}else if(s[i] == 'A'){
+				count_a++;
+			}else if(s[i] == 'T'){
+				count_t++;
+				pos_t = i;
+			}
+		}
+		if(count_p+count_a+count_t<s.length() || count_p>1 || count_t>1 || pos_p*(pos_t-pos_p-1)!=s.length()-pos_t-1 || pos_t-pos_p<=1)
+		    flag[num] = 0;
+              //printf("NO\n");
+        else
+            flag[num] = 1;
+              //printf("YES\n");
+	}
+    for(int i = 0; i< n;i++){
+    if(flag[i])
+        printf("YES\n");
+    else 
+        printf("NO\n");
+	}
     return 0;
 }
--- Leet_Code/LeetCode0042.cpp
+++ Leet_Code/LeetCode0042.cpp
@@ -1,40 +1,47 @@
+/*
+接雨水
+*/
 #include <iostream>
 #include <vector>
 using namespace std;
-//有问题
-class Solution{
+class Solution {
 public:
-    int trap(vector<int>& height){
-        int size = height.size();
-        int result = 0;
-        if(size <= 0){
-            return result;
-        }//if
-        int leftMax[size];
-        int rightMax[size];
-        leftMax[0] = 0;
-        rightMax[size - 1] = 0;//
-        //对于第i个元素，左右最大值
-        for(int i = 1;i < size; ++i){
-            leftMax[i] = max(leftMax[i-1],height[i-1]);
-            rightMax[size-1-i] = max(rightMax[size-i],height[size-i]);
-
-        } //for
-        //最大集雨水
-        int h;
-        for(int i = 0; i < size;++i){
-            h = min(leftMax[i],rightMax[i]);
-            if(h > height[i]){
-                result += h - height[i];
-            }//if
-        }//for
-        return result;
+    int trap(vector<int>& height) {
+        int len = height.size();
+        int sum=0;//储水量
+        int left=0,left_max=0,right=len-1,right_max=len-1;
+        for(int i=0;i<len;i++)
+        {
+            if(height[left] <= height[i])
+            {
+                for(int j = left; j < i; j++){
+                    sum = sum + height[left] - height[j];
+                }
+                left_max = i;
+                left = i;
+            }
+        }
+        printf("第一次%d\n",height[left]);
+        for(int i = len - 1; i >= left_max; i--)//右侧开始计算终止条件是到达左侧计算的最大值
+        {
+            if(height[right] <= height[i])//右侧同样可能遇到两个数组值相同
+            {
+                for(int j = right; j > i; j--){
+                    sum = sum + height[right] - height[j];
+                }
+                right_max = i;
+                right = i;
+            }
+        }
+        printf("第二次%d\n",height[right]);
+        return sum;
     }
 };
 int main()
 {
     Solution solution;
-    vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
+    // vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
+    vector<int> height = {4,2,0,3,2,5};
     cout << solution.trap(height) << endl;
     return 0;
 }
